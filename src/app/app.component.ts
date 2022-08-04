d89efa067d9496428247862a597d1e0a9b668bd3
vrundanawal <nawal.vrunda@gmail.com>
2022-08-04 18:14:50 +0100

    Property Binding syntax

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'password-generator';
+  password = '';
+
   onButtonClick() {
+    this.password = 'Hello';
     console.log('Button was clicked');
   }
+  //we can pass the function as well to get the value on event handler
+  // getPassword() {
+  //   return this.password;
+  // }
 }

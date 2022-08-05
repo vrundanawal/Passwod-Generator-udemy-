c5cb35d22dc0e923836c95cddb97f4b6795f330c
vrundanawal <nawal.vrunda@gmail.com>
2022-08-05 10:13:20 +0100

    track the state and addtional Properties with event handling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,13 +8,39 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'password-generator';
   password = '';
+  includeLettes = false;
+  includeNumbers = false;
+  includeSymbols = false;
+
+  //define the methods to handle events on DOM
+  onChangeUseLetters() {
+    this.includeLettes = !this.includeLettes;
+  }
+
+  onChangeUseNumbers() {
+    this.includeNumbers = !this.includeNumbers;
+  }
+
+  onChangeUseSymbols() {
+    this.includeSymbols = !this.includeSymbols;
+  }
 
   onButtonClick() {
+    console.log(`About to generate a password with the following:
+    Include Letters : ${this.includeLettes}
+    Include Numbers : ${this.includeNumbers}
+    Include Symbols : ${this.includeSymbols}
+    `);
     this.password = 'Hello';
-    console.log('Button was clicked');
   }
+
   //we can pass the function as well to get the value on event handler
   // getPassword() {
   //   return this.password;
+  // }
+
+  //use string interplotion for a method as well
+  // getName() {
+  //   return 'Vrunda';
   // }
 }
